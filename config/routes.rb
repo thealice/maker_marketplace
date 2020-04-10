afb76cb688100816e36757fa6761b2150281d635
Alice Freda <alicefreda@gmail.com>
2020-04-10 16:24:45 -0700

    testing out customizing devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'shops#index'
   resources :shops
-  devise_for :users
+  devise_for :users,
+    :path_names => {
+    :sign_in => 'login',
+    :sign_out => 'logout',
+    :registration => 'register',
+    :sign_up => 'signup' }
+
   # For details on devise_for, see: https://www.rubydoc.info/github/plataformatec/devise/master/ActionDispatch/Routing/Mapper%3Adevise_for
   # Session routes for Authenticatable (default)
     # new_user_session GET    /users/sign_in                    {controller:"devise/sessions", action:"new"}
