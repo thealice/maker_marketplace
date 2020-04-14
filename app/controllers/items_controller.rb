a68f3b2b7f6c77b636295b0b7761f8f1109feb65
Alice Freda <alicefreda@gmail.com>
2020-04-14 15:43:26 -0700

    protect item editing and deleting when nested to shop owner and set nested index and item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def show
   end
 
   def index
+    if params[:shop_id]
+      @items = Shop.find(params[:shop_id]).items
+    else
+      @items = Item.all
+    end
   end
 
   def new
@@ -14,11 +20,39 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if params[:shop_id]
+      @shop = Shop.find(params[:shop_id])
+      if owner_of(@shop)
+        render :edit
+      else
+        redirect_to shop_item_path(params[:shop_id], @item.id), alert: 'Only the shop owner can edit this item.'
+      end
+    end
   end
 
   def update
   end
 
   def destroy
+    if params[:shop_id]
+      @shop = Shop.find(params[:shop_id])
+      if owner_of(@shop)
+        @item.destroy
+        redirect_to items_path, notice: 'Item successfully deleted.'
+      else
+        redirect_to shop_item_path(params[:shop_id], @item.id), alert: 'Only the shop owner can delete this item.'
+      end
+    end
   end
+
+  private
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:id, :name, :description, :price, :quantity, :thumbnail, :category_ids [])
+    end
+
 end
