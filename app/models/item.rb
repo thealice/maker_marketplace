1dbcd5724dcf282e2c375f3505898f855652ed98
Alice Freda <alicefreda@gmail.com>
2020-04-14 20:10:25 -0700

    destroy item_categories when deleting an item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :shop
-  has_many :item_categories
+  has_many :item_categories, dependent: :destroy
   has_many :categories, through: :item_categories
 
   has_rich_text :description
