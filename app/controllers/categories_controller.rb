affa202e6f7335305e3cf74c7a6cf3fd52286c45
Alice Freda <alicefreda@gmail.com>
2020-04-15 17:22:06 -0700

    add show and index actions to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
+  
   def show
-    @category = Category.find_by_id(params[:id])
     @items = @category.items.all
   end
+
+  def index
+    @categories = Category.all.sort
+  end
+
+  private
+
+  def set_category
+    @category = Category.find_by_id(params[:id]) if Category.find_by_id(params[:id])
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
+    validates_presence_of :name
     has_many :item_categories
-    has_many :products, through: :item_categories
+    has_many :items, through: :item_categories
+
+    def self.with_items_alph
+        @categories = Category.all.reject do |c|
+            c.items.count < 1
+        end
+        @categories.sort_by {|c| c.name}
+    end
+
+
 end
