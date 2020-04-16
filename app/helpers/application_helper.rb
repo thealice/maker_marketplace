30de177a0b9180eadd33aca3a5ce5063ddc1d84d
Alice Freda <alicefreda@gmail.com>
2020-04-16 14:42:48 -0700

    add stripe connect button to user registration form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
     def stripe_url
       "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe)[:connect_client_id]}&scope=read_write"
     end
+
+    def stripe_connect_button
+        link_to stripe_url, class: "btn-stripe-connect" do
+          content_tag :span, "Connect with Stripe"
+        end
+    end
+
 end
