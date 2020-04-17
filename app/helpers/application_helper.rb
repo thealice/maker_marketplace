cdcf0634fa78a0ba6c3d05aa748e913e0b303849
Alice Freda <alicefreda@gmail.com>
2020-04-17 14:32:04 -0700

    gravatar and markdown formatting helpers for conversations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,24 @@
 module ApplicationHelper
-    def stripe_url
-      "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe)[:connect_client_id]}&scope=read_write"
-    end
 
-    def stripe_connect_button
-        link_to stripe_url, class: "btn-stripe-connect" do
-          content_tag :span, "Connect with Stripe"
-        end
-    end
+  def gravatar_for(user, options = { size: 200})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "border-radius-50")
+  end
+
+  def markdown_to_html(text)
+      Kramdown::Document.new(text, input: "GFM").to_html
+  end
+
+  def stripe_url
+    "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe)[:connect_client_id]}&scope=read_write"
+  end
+
+  def stripe_connect_button
+      link_to stripe_url, class: "btn-stripe-connect" do
+        content_tag :span, "Connect with Stripe"
+      end
+  end
 
 end
