146ed87885dda90ac91eaa2fb4ce1dfd1abf4217
Alice Freda <alicefreda@gmail.com>
2020-04-17 14:12:16 -0700

    add item_owner helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
     # see https://nvisium.com/blog/2014/09/10/understanding-protectfromforgery.html
     protect_from_forgery with: :exception
-    helper_method :owner_of, :admin?
+    helper_method :owner_of, :item_owner, :admin?
 
-    def owner_of(resource)
-        user_signed_in? && resource.user_id == current_user.id
+    def owner_of(shop)
+        user_signed_in? && shop.user_id == current_user.id
+    end
+
+    def item_owner(item)
+        user_signed_in? && item.shop.user_id == current_user.id
     end
     
     def admin?
