b76c7a0874430b9a34f28b5d0c40d36dc7833218
Alice Freda <alicefreda@gmail.com>
2020-04-17 19:45:20 -0700

    fix error where message form wasn't displaying

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -13,6 +13,7 @@ class ConversationsController < ApplicationController
         # create! raises exception if not saved
         @conversation = Conversation.create!(conversation_params)
       end
+      redirect_to conversation_messages_path(@conversation)
     end
   
     private
