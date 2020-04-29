fc947f16de4bbb73e05e80cbe0b9c2a47718444e
Alice Freda <alicefreda@gmail.com>
2020-04-29 12:55:10 -0700

    add a few comments to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,10 +3,13 @@ class Conversation < ApplicationRecord
     belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
   
     has_many :messages
-  
+
+    # Users can't start a conversation with themselves
     validates_uniqueness_of :sender_id, scope: :recipient_id
 
-    # Allows users to have one continuous conversation.
+    # Allows us to check whether there is already a conversation between two users
+    # so the same two users can have one continuous conversation rather than creating
+    # a new one each time.
     scope :between, -> (sender_id, recipient_id) do
       where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", sender_id, recipient_id, recipient_id, sender_id)
     end
